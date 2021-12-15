76149ade22775e9a2c7d8243f06e224304af28f6
Dale Limoges <limogesdale@outlook.com>
2021-12-15 13:10:52 -0500

    config02 avancée, reste a terminer la présentation

--- src/assets/scripts/presentation.js
+++ src/assets/scripts/presentation.js
@@ -1,4 +1,18 @@
-import 'bootstrap-icons/font/bootstrap-icons.scss';
+import '../styles/presentation.scss';
+import Reveal from 'reveal.js/dist/reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+
+
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    center: true,
+    hash: true,
+    embedded: false,
+    plugins: [ Markdown, Highlight ]
+});
+
 console.log('Script exclusif à la page présentation');
 
 // Change the size of our presentation
---
+++ webpack.config.commun.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    entry: {
+        main: path.resolve(__dirname, 'src/assets/scripts/main.js'),
+        presentation: path.resolve(__dirname, 'src/assets/scripts/presentation.js'),
+        pageconstruction: path.resolve(__dirname, 'src/assets/scripts/pageconstruction.js'),
+      },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        assetModuleFilename: 'assets/[name][ext]',
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
+            excludeChunks: [ 'pageconstruction' ]
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'page-en-construction.html', 
+            template: path.resolve(__dirname, 'src/page-en-construction.html'), 
+            chunks: [ 'main', 'pageconstruction' ]
+        }), 
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env'],
+                    },
+                  },
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, 
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name][ext]'
+                } 
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot)$/, 
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                } 
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    
+    
+};
