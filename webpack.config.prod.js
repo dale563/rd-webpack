aa808b388e7de66b906e24350359050508a499ab
Dale Limoges <limogesdale@outlook.com>
2022-02-02 16:33:09 -0500

    ficher de configuration rempli sans optimisation

---
+++ webpack.config.prod.js
@@ -0,0 +1,15 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.config.commun');
+const { merge } = require('webpack-merge');
+module.exports = merge(common, {
+    mode: 'production',
+    plugins: [
+        new MiniCssExtractPlugin({
+            linkType: 'text/css',
+            filename: 'assets/styles/[name]-[contenthash].css'
+         })
+    ],
+    output: {
+        filename: 'assets/scripts/[name]-[contenthash].bundle.js',
+    },
+});
